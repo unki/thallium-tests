96c0bed0e5805459ae380a519caca1bf006fda7f
Andreas Unterkircher <unki@netshadow.net>
2016-08-15 09:38:10 +0200

    add run_docs.sh

---
+++ run_docs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# APIGEN
+#
+
+APIGEN="/usr/bin/apigen"
+#APIGEN="strace -f -s 256 /usr/bin/apigen"
+
+DIRNAME=$(dirname $0)
+DIRNAME=$(realpath ${DIRNAME})
+GITORIGIN="devel"
+
+OUTPUT="${DIRNAME}/docs"
+TITLE="Thallium Test Suite Documentation"
+SOURCE="tests"
+
+if [ -d ${OUTPUT} ]; then
+   rm -r ${OUTPUT}
+fi
+
+if [ ! -d ${SOURCE} ]; then
+   echo "${SOURCE} already exists!"
+   exit 1
+fi
+
+${APIGEN} \
+   generate \
+   --debug \
+   --source="${SOURCE}" \
+   --destination="${OUTPUT}" \
+   --title="${TITLE}" \
+   --todo \
+   --tree \
+   --template-theme="bootstrap"
+
+RETVAL=$?
+
+if [ "x${RETVAL}" != "x0" ]; then
+   echo "${APIGEN} exited with exit code ${RETVAL}!"
+   exit 1
+fi
