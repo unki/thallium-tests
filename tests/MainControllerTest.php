35a4f4ac5c2b7a6575348292ce5ceb96b7e473a8
Andreas Unterkircher <unki@netshadow.net>
2016-08-15 09:09:45 +0200

    MainControllerTest, add annotations

--- tests/MainControllerTest.php
+++ tests/MainControllerTest.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * This file is part of Thallium.
+ *
+ * Thallium, a PHP-based framework for web applications.
+ * Copyright (C) <2015-2016> <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+/**
+ * This file is used to test Thalliums MainController.
+ *
+ * @license AGPL3
+ * @copyright 2015-2016 Andreas Unterkircher <unki@netshadow.net>
+ * @author Andreas Unterkircher <unki@netshadow.net>
+ */
 namespace Thallium\Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -7,6 +31,11 @@ use PHPUnit\Framework\TestCase;
 class MainControllerTest extends TestCase
 {
     /**
+     * tries instance the MainController.
+     *
+     * @params none
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @expectedException \Thallium\Controllers\ExceptionController
      */
     public function testNaked()
@@ -16,6 +45,12 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * instances the MainController in install mode (= install database schema).
+     * All other tests are depending on this method.
+     *
+     * @params none
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testNaked
      */
     public function testInit()
@@ -26,15 +61,25 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test of the inTestMode() method that should return true as we are
+     * calling it from phpunit.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testInTestMode(\Thallium\Controllers\MainController $controller)
     {
         $this->assertTrue($controller->inTestMode());
-        return $controller;
     }
 
     /**
+     * a test of the startup() method that should return true on a successful run.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testStartup(\Thallium\Controllers\MainController $controller)
@@ -43,6 +88,12 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the runBackgroundJobs() method, which normally should return true
+     * in this empty instance.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testRunBackgroundJobs(\Thallium\Controllers\MainController $controller)
@@ -51,14 +102,26 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the setVerbosity() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
+     * @expectedException \Thallium\Controllers\ExceptionController
      */
     public function testSetVerbosity(\Thallium\Controllers\MainController $controller)
     {
+        $this->expectException($controller->setVerbosity('loud'));
         $this->assertTrue($controller->setVerbosity(LOG_DEBUG));
     }
 
     /**
+     * a test for the isValidId() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testIsValidId(\Thallium\Controllers\MainController $controller)
@@ -70,18 +133,28 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the isValidModel() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
+     * @expectedException \Thallium\Controllers\ExceptionController
      */
-    public function isValidModel(\Thallium\Controllers\MainController $controller)
+    public function testIsValidModel(\Thallium\Controllers\MainController $controller)
     {
-        $this->assertFalse($controller->isValidModel());
-        $this->assertFalse($controller->isValidModel(''));
+        $this->expectException($controller->isValidModel(''));
         $this->assertFalse($controller->isValidModel('1'));
         $this->assertFalse($controller->isValidModel(1));
         $this->assertTrue($controller->isValidModel('MessageBusModel'));
     }
 
     /**
+     * a test for the isValidGuidSyntax() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testIsValidGuidSyntax(\Thallium\Controllers\MainController $controller)
@@ -89,29 +162,41 @@ class MainControllerTest extends TestCase
         $this->assertFalse($controller->isValidGuidSyntax(''));
         $this->assertFalse($controller->isValidGuidSyntax('1'));
         $this->assertFalse($controller->isValidGuidSyntax(1));
-        $this->assertTrue(
-            $controller->isValidGuidSyntax('0123456789012345678901234567890123456789012345678901234567890123')
-        );
+
+        $this->assertTrue($controller->isValidGuidSyntax(
+            '0123456789012345678901234567890123456789012345678901234567890123'
+        ));
     }
 
     /**
+     * a test for the parseId() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @expectedException \Thallium\Controllers\ExceptionController
      */
     public function testParseId(\Thallium\Controllers\MainController $controller)
     {
-        $this->assertFalse($controller->parseId(''));
+        $this->expectException($controller->parseId(''));
         $this->assertFalse($controller->parseId('1'));
         $this->assertFalse($controller->parseId(1));
         $this->assertFalse($controller->parseId('message'));
         $this->assertFalse($controller->parseId('message-1'));
         $this->assertFalse($controller->parseId('message-1-2'));
-        $this->assertTrue(
-            $controller->parseId('message-1-0123456789012345678901234567890123456789012345678901234567890123')
-        );
+
+        $this->assertTrue($controller->parseId(
+            'message-1-0123456789012345678901234567890123456789012345678901234567890123'
+        ));
     }
 
     /**
+     * a test for the createGuid() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testCreateGuid(\Thallium\Controllers\MainController $controller)
@@ -121,6 +206,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the loadModel() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @expectedException \Thallium\Controllers\ExceptionController
      */
@@ -137,6 +227,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the checkUpgrade() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testCheckUpgrade(\Thallium\Controllers\MainController $controller)
@@ -145,15 +240,24 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the loadController() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testLoadController(\Thallium\Controllers\MainController $controller)
     {
-        $retval = $controller->loadController('MessageBusController', 'mbus');
-        $this->assertTrue($retval);
+        $this->assertTrue($controller->loadController('MessageBusController', 'mbus'));
     }
 
     /**
+     * a test for the getProcessUserId() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testGetProcessUserId(\Thallium\Controllers\MainController $controller)
@@ -165,6 +269,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getProcessGroupId() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testGetProcessGroupId(\Thallium\Controllers\MainController $controller)
@@ -176,6 +285,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getProcessUserName() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testGetProcessUserName(\Thallium\Controllers\MainController $controller)
@@ -184,6 +298,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getProcessGroupName() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testGetProcessGroupName(\Thallium\Controllers\MainController $controller)
@@ -192,6 +311,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the processRequestMessages() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testProcessRequestMessages(\Thallium\Controllers\MainController $controller)
@@ -200,6 +324,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getNamespacePrefix() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testGetNamespacePrefix(\Thallium\Controllers\MainController $controller)
@@ -208,6 +337,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the setNamespacePrefix() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @expectedException \Thallium\Controllers\ExceptionController
      */
@@ -219,6 +353,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getRegisteredModels() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testGetRegisteredModels(\Thallium\Controllers\MainController $controller)
@@ -230,6 +369,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the registerModel() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @expectedException \Thallium\Controllers\ExceptionController
      */
@@ -244,6 +388,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the isRegisteredModel() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @depends testRegisterModel
      * @expectedException \Thallium\Controllers\ExceptionController
@@ -262,6 +411,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getModelByNick() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @depends testRegisterModel
      * @expectedException \Thallium\Controllers\ExceptionController
@@ -274,6 +428,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the isBelowDirectory() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @expectedException \Thallium\Controllers\ExceptionController
      */
@@ -293,6 +452,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the flushOutputBufferToLog() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      */
     public function testFlushOutputBufferToLog(\Thallium\Controllers\MainController $controller)
@@ -301,6 +465,11 @@ class MainControllerTest extends TestCase
     }
 
     /**
+     * a test for the getFullModelName() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
      * @depends testInit
      * @depends testRegisterModel
      * @depends testSetNamespacePrefix
