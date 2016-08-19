bd63b930a5e80a6011f702b5340668da0dd8566d
Andreas Unterkircher <unki@netshadow.net>
2016-08-19 20:53:59 +0200

    add RequirementsControllerTest.php

---
+++ tests/RequirementsControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of Thallium.
+ *
+ * Thallium, a PHP-based framework for web applications.
+ * Copyright (C) <2015-2016> <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+/**
+ * This file is used to test Thalliums RequirementsController.
+ *
+ * @license AGPL3
+ * @copyright 2015-2016 Andreas Unterkircher <unki@netshadow.net>
+ * @author Andreas Unterkircher <unki@netshadow.net>
+ */
+namespace Thallium\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class RequirementsControllerTest extends TestCase
+{
+    protected $test_job;
+
+    /**
+     * instances the RequirementsController.
+     * All other tests are depending on this method.
+     *
+     * @params none
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
+     */
+    public function testConstruct()
+    {
+        $controller = new \Thallium\Controllers\RequirementsController;
+        $this->assertInstanceOf('\Thallium\Controllers\RequirementsController', $controller);
+
+        return $controller;
+    }
+
+    /**
+     * a test for the check() method.
+     *
+     * @params object $controller
+     * @returns void
+     * @throws \Thallium\Controllers\ExceptionController
+     * @depends testConstruct
+     */
+    public function testCheck(\Thallium\Controllers\RequirementsController $controller)
+    {
+        $this->assertTrue($controller->check());
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
